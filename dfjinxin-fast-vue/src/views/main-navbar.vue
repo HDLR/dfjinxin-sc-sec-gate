ad064303db3cc47d66c1ce13ab9a11a73b429692
HDLR <492587402@qq.com>
2019-05-18 11:47:39 +0800

    修改注释

--- dfjinxin-fast-vue/src/views/main-navbar.vue
+++ dfjinxin-fast-vue/src/views/main-navbar.vue
@@ -3,7 +3,7 @@
     <div class="site-navbar__header">
       <h1 class="site-navbar__brand" @click="$router.push({ name: 'home' })">
         <a class="site-navbar__brand-lg" href="javascript:;">dfjinxin权限管理</a>
-        <a class="site-navbar__brand-mini" href="javascript:;">jinxin</a>
+        <a class="site-navbar__brand-mini" href="javascript:;">管理</a>
       </h1>
     </div>
     <div class="site-navbar__body clearfix">
