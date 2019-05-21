69be2988874bc83a0b70f5c72ac3bd179adb53d8
HDLR <492587402@qq.com>
2019-05-21 23:43:23 +0800

    test

---
+++ dfjinxin-modules/dfjinxin-test/src/main/java/com/dfjinxin/test/test/TestController.java
@@ -0,0 +1,13 @@
+package com.dfjinxin.test.test;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @RequestMapping("/test")
+    public String test(){
+        return "test context";
+    }
+}
---
+++ dfjinxin-modules/dfjinxin-test/src/test/java/com/dfjinxin/test/DfjinxinTestApplicationTests.java
@@ -0,0 +1,16 @@
+package com.dfjinxin.test;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DfjinxinTestApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
