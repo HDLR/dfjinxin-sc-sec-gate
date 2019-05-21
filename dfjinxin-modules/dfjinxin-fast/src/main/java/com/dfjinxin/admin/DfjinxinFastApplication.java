7cb5b0ac48f79da2b82b70e293790764053de818
HDLR <492587402@qq.com>
2019-05-21 23:03:32 +0800

    添加@EnableCircuitBreaker

--- dfjinxin-modules/dfjinxin-fast/src/main/java/com/dfjinxin/admin/DfjinxinFastApplication.java
+++ dfjinxin-modules/dfjinxin-fast/src/main/java/com/dfjinxin/admin/DfjinxinFastApplication.java
@@ -15,15 +15,17 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableDfjinxinAuthClient
 @EnableDiscoveryClient
-@EnableFeignClients({"com.dfjinxin.auth.client.feign"})
+@EnableFeignClients({"com.dfjinxin.auth.client.feign", "com.dfjinxin.admin.feign"})
 @EnabledfjinxinCache
 @EnableDfjinxinCommon
+@EnableCircuitBreaker
 @EnableAutoConfiguration
 public class DfjinxinFastApplication {
 
