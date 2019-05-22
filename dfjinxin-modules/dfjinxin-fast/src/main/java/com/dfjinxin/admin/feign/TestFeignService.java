1bc1f6dc9b7e122b0b69c8a5ff6ec987f0ffdd8a
HDLR <492587402@qq.com>
2019-05-22 11:11:48 +0800

    feign远程测试

--- dfjinxin-modules/dfjinxin-fast/src/main/java/com/dfjinxin/admin/feign/TestFeignService.java
+++ dfjinxin-modules/dfjinxin-fast/src/main/java/com/dfjinxin/admin/feign/TestFeignService.java
@@ -1,12 +1,15 @@
 package com.dfjinxin.admin.feign;
 
+import com.dfjinxin.common.msg.ObjectRestResponse;
+import com.dfjinxin.common.msg.R;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-@FeignClient(value = "dfjinxin-test",fallback = TestFeignServiceFallback.class)
+//@FeignClient(value = "dfjinxin-test",fallback = TestFeignServiceFallback.class)
+@FeignClient(value = "dfjinxin-test",fallbackFactory = TestFeignServiceFallback.class)
 public interface TestFeignService {
 
-    @RequestMapping(value="/test",method = RequestMethod.GET)
-    public String test();
+    @RequestMapping(value="/test1",method = RequestMethod.GET)
+    public ObjectRestResponse<String> test();
 }
--- dfjinxin-modules/dfjinxin-fast/src/main/java/com/dfjinxin/admin/feign/TestFeignServiceFallback.java
+++ dfjinxin-modules/dfjinxin-fast/src/main/java/com/dfjinxin/admin/feign/TestFeignServiceFallback.java
@@ -1,12 +1,31 @@
 package com.dfjinxin.admin.feign;
 
 import com.dfjinxin.common.exception.auth.FeignException;
+import com.dfjinxin.common.msg.ObjectRestResponse;
+import com.dfjinxin.common.msg.R;
+import feign.hystrix.FallbackFactory;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Component;
 
 @Component
-public class TestFeignServiceFallback implements TestFeignService {
+public class TestFeignServiceFallback implements FallbackFactory<TestFeignService> {
 
-    public String test(){
-        throw new FeignException("访问后台管理系统dfjinxin-admin异常");
+    public static final String ERR_MSG = "dfjinxin-test接口暂时不可用: ";
+    @Override
+    public TestFeignService create(Throwable throwable) {
+        String msg = throwable == null ? "" : throwable.getMessage();
+        if (!StringUtils.isEmpty(msg)) {
+            System.out.println("异常msg================" + msg);
+        }
+        return new TestFeignService() {
+            @Override
+            public ObjectRestResponse test() {
+//                return ERR_MSG + msg;
+                ObjectRestResponse res = new ObjectRestResponse();
+                res.setStatus(500);
+                res.setMessage(ERR_MSG + msg);
+                return res;
+            }
+        };
     }
 }
